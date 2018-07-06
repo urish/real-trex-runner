aa1a05a72eadd51466a7abf8e3ab573530f22d04
Uri Shaked <uri@urishaked.com>
2018-07-06 22:45:02 +0300

    fix(clock): clear display when updating clock time
    otherwise digits become blurry over time

--- firmware/src/clock.js
+++ firmware/src/clock.js
@@ -1,40 +1,49 @@
 /**
  * Real Life Lonely T-Rex Game Firmware
- * 
+ *
  * Copyright (C) 2017, 2018, Uri Shaked
  */
 
 const assets = require('./assets');
 const display = require('./display');
 
-let updateTimer = null; 
+let updateTimer = null;
 
 function displayString(s, x, y, charWidth, charHeight, spacing, prefix) {
   s.split('').forEach((c, i) => {
     if (c !== ' ') {
-      display.writeChar(assets[(prefix || '') + c], charWidth, charHeight, y - i * (charHeight + spacing), x);
+      display.writeChar(
+        assets[(prefix || '') + c],
+        charWidth,
+        charHeight,
+        y - i * (charHeight + spacing),
+        x
+      );
     }
   });
 }
 
 function zeroPad(val) {
-  return ((val < 10) ? '0' : '') + val.toString();
+  return (val < 10 ? '0' : '') + val.toString();
 }
 
 function displayTime(time) {
-  const top = 40;
-  let hrs = zeroPad(time.getHours());
-  let mins = zeroPad(time.getMinutes());
-  display.fillMemory(0xff);
-  displayString(hrs, top, 220, 48, 38, 10);
-  display.writeChar(assets.colon, 48, 11, 220 - 74, top);
-  displayString(mins, top, 220 - 130, 48, 38, 10);
-  return display.displayFrame();
+  return display.cls().then(() => {
+    const top = 40;
+    let hrs = zeroPad(time.getHours());
+    let mins = zeroPad(time.getMinutes());
+    display.fillMemory(0xff);
+    displayString(hrs, top, 220, 48, 38, 10);
+    display.writeChar(assets.colon, 48, 11, 220 - 74, top);
+    displayString(mins, top, 220 - 130, 48, 38, 10);
+    return display.displayFrame();
+  });
 }
 
 function clockTask() {
   const date = new Date();
-  const timeToUpdate = 60000 - (date.getSeconds() * 1000 + date.getMilliseconds()) + 10;
+  const timeToUpdate =
+    60000 - (date.getSeconds() * 1000 + date.getMilliseconds()) + 10;
   display.registerUpdate(() => displayTime(date));
   updateTimer = setTimeout(clockTask, timeToUpdate);
 }
