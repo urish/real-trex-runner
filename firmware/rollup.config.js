fc68f986ef744f10787eebd6a300e21b228f42e9
Uri Shaked <uri@urishaked.com>
2019-08-22 15:38:45 +0300

    chore(firmware): upgrade rollup and plugins

--- firmware/rollup.config.js
+++ firmware/rollup.config.js
@@ -1,17 +1,19 @@
 import commonjs from 'rollup-plugin-commonjs';
-import minify from 'rollup-plugin-minify-es';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/trex.js',
   output: {
     file: 'dist/firmware.js',
     format: 'cjs',
+    banner: 'const module={}',
+    interop: false,
   },
-  external: ['Flash', 'NRF52LL', 'Storage'],
-  intro: 'const module={}',
   plugins: [
-    commonjs(),
-    minify({
+    commonjs({
+      ignore: ['NRF52LL', 'Storage']
+    }),
+    terser({
       output: {
         max_line_len: true,
         semicolons: false,
